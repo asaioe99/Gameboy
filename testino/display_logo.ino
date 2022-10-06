eb779b5878a271be45335f9fb9cb82f2874d5a09
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-06 15:00:34 +0900

    Update display_logo.ino

--- testino/display_logo.ino
+++ testino/display_logo.ino
@@ -1,14 +1,14 @@
 /*
 カートリッジpin割り当て表(カートリッジ左から1pin)
 https://gbdev.gg8.se/wiki/articles/DMG_Schematics
-1: Vcc	   9: a3 	17: a11	 25: d3
-2: CLK?  10: a4	 18: a12	 26: d4
-3: /WR	  11: a5	 19: a13	 27: d5
-4: /RD	  12: a6	 20: a14	 28: d6
-5: /CS   13: a7	 21: a15	 29: d7
-6: a0	   14: a8	 22: d0	  30: /RST
-7: a1	   15: a9	 23: d1	  31: Vin
-8: a2	   16: a10	24: d2	  32: GND
+1: Vcc   9: a3 	 17: a11  25: d3
+2: CLK? 10: a4   18: a12  26: d4
+3: /WR  11: a5   19: a13  27: d5
+4: /RD  12: a6   20: a14  28: d6
+5: /CS  13: a7   21: a15  29: d7
+6: a0	  14: a8   22: d0	  30: /RST
+7: a1	  15: a9   23: d1	  31: Vin
+8: a2	  16: a10	 24: d2	  32: GND
 */
 // アドレスバスの指定
 void set_addr(uint16_t address) { 
