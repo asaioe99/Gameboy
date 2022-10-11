d7cba69c1fa020805f54252ab33477c04bdfb7a9
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-11 12:37:23 +0900

    Update opcode.ino

--- opcode.ino
+++ opcode.ino
@@ -41,3 +41,51 @@ void LD_L_n() {
   cc += 8;
   pc++;
 }
+// 0x7F
+void LD_A_A() {
+    A = A;
+    cc +=4;
+    pc++;
+}
+// 0x78
+void LD_A_B() {
+    A = B;
+    cc +=4;
+    pc++;
+}
+// 0x79
+void LD_A_C() {
+    A = C;
+    cc +=4;
+    pc++;
+}
+// 0x7A
+void LD_A_D() {
+    A = D;
+    cc +=4;
+    pc++;
+}
+// 0x7B
+void LD_A_E() {
+    A = E;
+    cc +=4;
+    pc++;
+}
+// 0x7C
+void LD_A_H() {
+    A = H;
+    cc +=4;
+    pc++;
+}
+// 0x7D
+void LD_A_L() {
+    A = L;
+    cc +=4;
+    pc++;
+}
+// 0x7E
+void LD_A_lHLl() {
+    A = get_byte((H << 4) + L);
+    cc +=8;
+    pc++;
+}
