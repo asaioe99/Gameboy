338332e9814b07da7da41f70da81f00a23779880
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-13 16:03:20 +0900

    Update execute.ino

--- execute.ino
+++ execute.ino
@@ -2,6 +2,79 @@ void execute(uint8_t pc) {
   uint8_t code = fetch(pc);
 
   switch(code) {
+    case 0xCB: // prefix
+      //cc += 4;
+      code = fetch(++pc);
+      switch(code) {
+        case 0x10:
+        case 0x11:
+        case 0x12:
+        case 0x13:
+        case 0x14:
+        case 0x15:
+        case 0x17:
+          RL_r(code);
+          break;
+        case 0x40:
+        case 0x41:
+        case 0x42:
+        case 0x43:
+        case 0x44:
+        case 0x45:
+        case 0x47:
+        case 0x48:
+        case 0x49:
+        case 0x4A:
+        case 0x4B:
+        case 0x4C:
+        case 0x4D:
+        case 0x4F:
+        case 0x50:
+        case 0x51:
+        case 0x52:
+        case 0x53:
+        case 0x54:
+        case 0x55:
+        case 0x57:
+        case 0x58:
+        case 0x59:
+        case 0x5A:
+        case 0x5B:
+        case 0x5C:
+        case 0x5D:
+        case 0x5F:
+        case 0x60:
+        case 0x61:
+        case 0x62:
+        case 0x63:
+        case 0x64:
+        case 0x65:
+        case 0x67:
+        case 0x68:
+        case 0x69:
+        case 0x6A:
+        case 0x6B:
+        case 0x6C:
+        case 0x6D:
+        case 0x6F:
+        case 0x70:
+        case 0x71:
+        case 0x72:
+        case 0x73:
+        case 0x74:
+        case 0x75:
+        case 0x77:
+        case 0x78:
+        case 0x79:
+        case 0x7A:
+        case 0x7B:
+        case 0x7C:
+        case 0x7D:
+        case 0x7F:
+          BIT(code);
+          break;
+      }
+      break;
     case 0x06: //LD_B_n
     case 0x0E: //LD_C_n
     case 0x16: //LD_D_n
