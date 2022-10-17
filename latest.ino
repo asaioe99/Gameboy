e9acf2683d6223a331e7d6acca732f034d6c37c7
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-17 18:40:36 +0900

    Update latest.ino

--- latest.ino
+++ latest.ino
@@ -12,7 +12,7 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RS
 
 // bootstrap（実物のため、そのままは掲載不可）
 uint8_t bootstrap[] = {
-  //   0     1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
+  // 0     1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
   0x31, 0xFE, 0xFF, 0xAF, 0x21, 0xFF, 0x9F, 0x32, 0xCB, 0x7C, 0x20, 0xFB, 0x21, 0x26, 0xFF, 0x0E, // 0
   0x11, 0x3E, 0x80, 0x32, 0xE2, 0x0C, 0x3E, 0xF3, 0xE2, 0x32, 0x3E, 0x77, 0x77, 0x3E, 0xFC, 0xE0, // 1
   0x47, 0x11, 0x04, 0x01, 0x21, 0x10, 0x80, 0x1A, 0xCD, 0x95, 0x00, 0xCD, 0x96, 0x00, 0x13, 0x7B, // 2
@@ -41,7 +41,7 @@ uint8_t D;
 uint8_t E;
 uint8_t H;
 uint8_t L;
-uint16_t cc;
+uint32_t cc;
 
 uint8_t oam[0xa0];
 uint8_t io[0x80];
@@ -104,7 +104,7 @@ byte get_ram_byte(uint16_t address) {
   set_addr(address);
   PORTL = B00000001;
   delayMicroseconds(10);
-  byte result = PINF;
+  uint8_t result = PINF;
   PORTL = B00000111;
   delayMicroseconds(10);
   return result;
@@ -274,25 +274,25 @@ void write_ram_bank(uint8_t bank) {
 uint8_t get_byte(uint16_t addr) {
   if (addr < 0x0100) {
     return bootstrap[addr];
-  } else if (addr < 0x4000) {
+  } else if (addr >= 0x0100 && addr < 0x4000) {
     return get_rom_byte(addr);
-  } else if (addr < 0x8000) {
+  } else if (addr >= 0x4000 && addr < 0x8000) {
     return get_rom_byte(addr);
-  } else if (addr < 0xA000) {
+  } else if (addr >= 0x8000 && addr < 0xA000) {
     return sram_rd(addr);
-  } else if (addr < 0xC000) {
+  } else if (addr >= 0xA000 && addr < 0xC000) {
     return get_ram_byte(addr);
-  } else if (addr < 0xE000) {
+  } else if (addr >= 0xC000 && addr < 0xE000) {
     return sram_rd(addr);
-  } else if (addr < 0xFE00) {  // Mirror of C000~DDFF
+  } else if (addr >= 0xE000 && addr < 0xFE00) {  // Mirror of C000~DDFF
     return sram_rd(addr);
-  } else if (addr < 0xFEA0) {  // Sprite attribute table (OAM)
+  } else if (addr >= 0xFE00 && addr < 0xFEA0) {  // Sprite attribute table (OAM)
     return oam[addr - 0xFE00];
-  } else if (addr < 0xFF00) {  // Not Usable
+  } else if (addr >= 0xFEA0 && addr < 0xFF00) {  // Not Usable
     return sram_rd(addr);
-  } else if (addr < 0xFF80) {  // I/O Register
+  } else if (addr >= 0xFF00 && addr < 0xFF80) {  // I/O Register
     return io[addr - 0xFF00];
-  } else if (addr < 0xFFFF) {  // High RAM stack
+  } else if (addr >= 0xFF80 && addr < 0xFFFF) {  // High RAM stack
     return hram[addr - 0xFF80];
   } else if (addr == 0xFFFF) { // Interrupt Enable register(IE)
     return ie;
@@ -302,22 +302,22 @@ uint8_t get_byte(uint16_t addr) {
 
 void put_byte(uint16_t addr, uint8_t data) {
   if (addr < 0x4000) {
-  } else if (addr < 0x8000) {
-  } else if (addr < 0xA000) {
+  } else if (addr >= 0x4000 && addr < 0x8000) {
+  } else if (addr >= 0x8000 && addr < 0xA000) {
     sram_wt(addr, data);
-  } else if (addr < 0xC000) {
+  } else if (addr >= 0xA000 && addr < 0xC000) {
     put_ram_byte(addr, data);
-  } else if (addr < 0xE000) {
+  } else if (addr >= 0xC000 && addr < 0xE000) {
     sram_wt(addr, data);
-  } else if (addr < 0xFE00) {  // Mirror of C000~DDFF
+  } else if (addr >= 0xE000 && addr < 0xFE00) {  // Mirror of C000~DDFF
     sram_wt(addr, data);
-  } else if (addr < 0xFEA0) {  // Sprite attribute table (OAM)
+  } else if (addr >= 0xFE00 && addr < 0xFEA0) {  // Sprite attribute table (OAM)
     oam[addr - 0xFE00] = data;
-  } else if (addr < 0xFF00) {  // Not Usable
+  } else if (addr >= 0xFEA0 && addr < 0xFF00) {  // Not Usable
     sram_wt(addr, data);
-  } else if (addr < 0xFF80) {  // I/O Register
+  } else if (addr >= 0xFF00 && addr < 0xFF80) {  // I/O Register
     io[addr - 0xFF00] = data;
-  } else if (addr < 0xFFFF) {  // High RAM
+  } else if (addr >= 0xFF80 && addr < 0xFFFF) {  // High RAM
     hram[addr - 0xFF80] = data;
   } else if (addr == 0xFFFF) { // Interrupt Enable register(IE)
   }
@@ -345,6 +345,7 @@ void setup() {
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE3);
   //  SPI.beginTransaction(settings);
+  display_rom_header();
 }
 
 void loop() {
@@ -353,16 +354,14 @@ void loop() {
   delayMicroseconds(10);
 
   //for debug
-  //display_rom_header();
+  
   //dump_rom_bank(1);
   //enable_ram();
   //write_ram_bank(0);
   //dump_ram_bank(0);
   //disable_ram();
 
-
-  //testdrawtext("PUIPUI", ST77XX_WHITE);
-  while (cc < 456 * (144 + 10)) {
+  while (cc < 70224) {
     execute();
     ppu();
   }
