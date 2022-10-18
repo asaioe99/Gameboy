45b6e890d51b2c99672ad3703fa74b6393615942
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-19 08:32:12 +0900

    Update execute.ino

--- execute.ino
+++ execute.ino
@@ -1,5 +1,5 @@
 void execute() {
-  uint8_t code = fetch(pc);
+  code = get_byte(pc);
 
   char buf[64];
   if (pc > 0xE0) {
@@ -9,76 +9,25 @@ void execute() {
   }
   cc_dec = 0;
   if (code == 0xCB) {
-    code = fetch(++pc);
-    switch (code) {
-      case 0x10:
-      case 0x11:
-      case 0x12:
-      case 0x13:
-      case 0x14:
-      case 0x15:
-      case 0x17:
-        RL_r(code);
-        return;
+    code = get_byte(++pc);
+    switch (code & 0b11000000) {
+      case 0b00000000: // 0x0*** ~ 0x3***
+        if ((code >= 0x10 && code <= 0x15) || code == 0x17) {
+          RL_r();
+          return;
+        }
         break;
-      case 0x40:
-      case 0x41:
-      case 0x42:
-      case 0x43:
-      case 0x44:
-      case 0x45:
-      case 0x47:
-      case 0x48:
-      case 0x49:
-      case 0x4A:
-      case 0x4B:
-      case 0x4C:
-      case 0x4D:
-      case 0x4F:
-      case 0x50:
-      case 0x51:
-      case 0x52:
-      case 0x53:
-      case 0x54:
-      case 0x55:
-      case 0x57:
-      case 0x58:
-      case 0x59:
-      case 0x5A:
-      case 0x5B:
-      case 0x5C:
-      case 0x5D:
-      case 0x5F:
-      case 0x60:
-      case 0x61:
-      case 0x62:
-      case 0x63:
-      case 0x64:
-      case 0x65:
-      case 0x67:
-      case 0x68:
-      case 0x69:
-      case 0x6A:
-      case 0x6B:
-      case 0x6C:
-      case 0x6D:
-      case 0x6F:
-      case 0x70:
-      case 0x71:
-      case 0x72:
-      case 0x73:
-      case 0x74:
-      case 0x75:
-      case 0x77:
-      case 0x78:
-      case 0x79:
-      case 0x7A:
-      case 0x7B:
-      case 0x7C:
-      case 0x7D:
-      case 0x7F:
-        BIT(code);
-        return;
+      case 0b01000000: // 0x4*** ~ 0x7***
+        if (code & 0b00000111 == 0b00000110) {
+          return;
+        } else {
+          BIT();
+          return;
+        }
+        break;
+      case 0b10000000: // 0x8*** ~ 0xB***
+        break;
+      case 0b11000000: // 0xC*** ~ 0xF***
         break;
     }
     return;
@@ -93,12 +42,12 @@ void execute() {
           } else if (code == 0x10) {
             return;
           } else {
-            JR_cc_d8(code);
+            JR_cc_d8();
             return;
           }
           break;
         case 0b00000001: // 1
-          LD_r16_d16(code);
+          LD_r16_d16();
           return;
           break;
         case 0b00000010:
@@ -117,14 +66,14 @@ void execute() {
           }
           break;
         case 0b00000011:
-          INC_rr(code);
+          INC_rr();
           return;
           break;
         case 0b00000100: // 4
           if (code == 0x34) {
             return;
           } else {
-            INC_r(code);
+            INC_r();
             return;
           }
           break;
@@ -132,16 +81,16 @@ void execute() {
           if (code == 0x35) {
             return;
           } else {
-            DEC_r(code);
+            DEC_r();
             return;
           }
           break;
         case 0b00000110:
           if (code == 0x06 || code == 0x16 || code == 0x26) {
-            LD_r_n(code);
+            LD_r_n();
             return;
           } else {
-            LD_HL_n(code);
+            LD_HL_n();
             return;
           }
           break;
@@ -155,7 +104,7 @@ void execute() {
           if (code == 0x08) {
             return;
           } else {
-            JR_cc_d8(code);
+            JR_cc_d8();
             return;
           }
           break;
@@ -177,19 +126,19 @@ void execute() {
           }
           break;
         case 0b00001011: // B
-          DEC_rr(code);
+          DEC_rr();
           return;
           break;
         case 0b00001100: // C
-          INC_r(code);
+          INC_r();
           return;
           break;
         case 0b00001101: // D
-          DEC_r(code);
+          DEC_r();
           return;
           break;
         case 0b00001110:
-          LD_r_n(code);
+          LD_r_n();
           return;
           break;
         case 0b00001111:
@@ -200,7 +149,7 @@ void execute() {
       if (code == 0x76) return; // HALT
       switch (code & 0b00000111) {
         case 0b110:
-          LD_r_HL(code);
+          LD_r_HL();
           break;
         case 0x70: //LD_(HL)_B
         case 0x71: //LD_(HL)_C
@@ -209,10 +158,10 @@ void execute() {
         case 0x74: //LD_(HL)_H
         case 0x75: //LD_(HL)_L
         case 0x77: //LD_(HL)_A
-          LD_HL_r(code);
+          LD_HL_r();
           break;
         default:
-          LD_r_r(code);
+          LD_r_r();
           break;
       }
       break;
@@ -220,26 +169,26 @@ void execute() {
       switch (code & 0b11110000) {
         case 0b10000000: //0x80
           if (code >= 0x80 && code <= 0x87) {
-            ADD_r(code);
+            ADD_r();
             return;
           }
           break;
         case 0b10010000: //0x90
           if (code >= 0x90 && code <= 0x97) {
-            SUB_r(code);
+            SUB_r();
             return;
           }
           break;
           break;
         case 0b10100000: //0xA0
           if (code >= 0xA8 && code <= 0xAF) {
-            XOR_r(code);
+            XOR_r();
             return;
           }
           break;
         case 0b10110000: //0xB0
           if (code >= 0xB8 && code <= 0xBF) {
-            CP_r(code);
+            CP_r();
             return;
           }
           break;
@@ -253,15 +202,15 @@ void execute() {
           } else if (code == 0xD0) {
             return;
           } else if (code == 0xE0) {
-            LDH_a8_a(code);
+            LDH_a8_a();
             return;
           } else {
-            LDH_a_a8(code);
+            LDH_a_a8();
             return;
           }
           break;
         case 0b00000001: // 1
-          POP_r16(code);
+          POP_r16();
           return;
           break;
         case 0b00000010: // 2
@@ -286,7 +235,7 @@ void execute() {
         case 0b00000100:
           break;
         case 0b00000101: //5
-          PUSH_r16(code);
+          PUSH_r16();
           return;
           break;
         case 0b00000110:
