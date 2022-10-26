91b87618deb7050ed2640327d7c41c3e486e8aa8
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-26 18:21:11 +0900

    Update ppu.ino

--- GameBoypico/ppu.ino
+++ GameBoypico/ppu.ino
@@ -3,38 +3,31 @@ void p_flag_update() {
   uint8_t *t_FF40 = io + 0x40;
   uint8_t *t_FF41 = io + 0x41;
   uint8_t *t_FF44 = io + 0x44;
-  uint8_t status_LCD = *t_FF41;
 
-  if ((*t_FF40 & 0x80) != 0x80) {
-    *t_FF40 = 0x00;
+  if ((*t_FF40 & 0b10000000) == 0b00000000) {
+    *t_FF44 = 0x00;
     scaline_counter = 456;
-    status_LCD &= 0b11111100;
-    status_LCD = ~(status_LCD & (1 << 0));
-    status_LCD = ~(status_LCD & (1 << 1));
-    *t_FF41 = status_LCD;
+    *t_FF41 &= 0b11111100;
+    *t_FF41 |= 0b00000001;
     return;
   }
   //現時点のmodeが何であるか取得し更新
-  status_LCD &= 0b11111100;              // mode0 デフォ
+  *t_FF41 &= 0b11111100;              // mode0 デフォ
   if (*t_FF44 >= 144) {                  // mode1
-    status_LCD |= 0b00000001;
+    *t_FF41 |= 0b00000001;
   } else {
     if (scaline_counter >= 376) {        // mode2
-      status_LCD |= 0b00000010;
+      *t_FF41 |= 0b00000010;
     } else if (scaline_counter >= 204) { // mode3
-      status_LCD |= 0b00000011;
+      *t_FF41 |= 0b00000011;
     }
   }
 
-  // put_byte(0xFF41, status_LCD);
-  *t_FF41 = status_LCD;
-
-  if (status_LCD == *(io + 0x45)) {
-    status_LCD = status_LCD | 0b00000100;
+  if (*t_FF44 == *(io + 0x45)) {
+    *t_FF41 = *t_FF41 | 0b00000100;
   } else {
-    status_LCD = status_LCD & 0b11111011;
+    *t_FF41 = *t_FF41 & 0b11111011;
   }
-  *t_FF41 = status_LCD;
 }
 
 void mode_0() {
@@ -51,20 +44,24 @@ void mode_2() {
 
 // bg&wnd フェッチ
 void mode_3() {
-  display_scanline();
+  //display_scanline();
+  //display_tile(0);
+
   return;
 }
 
 void mode_sw() {
-
   switch (*(io + 0x41) & 0b00000011) {
-    case 0b00: // mode 0
+    case 0b00000000: // mode 0
+      mode_0();
       break;
-    case 0b01: // mode 1
+    case 0b00000001: // mode 1
+      mode_1();
       break;
-    case 0b10: // mode 2
+    case 0b00000010: // mode 2
+      mode_2();
       break;
-    case 0b11: // mode 3
+    case 0b00000011: // mode 3
       mode_3();
       break;
   }
@@ -74,122 +71,95 @@ void mode_sw() {
 void ppu() {
   // flag等 アップデート
   p_flag_update();
+
+  if (*(io + 0x40) & 0b00001000 == 0b00001000) {
+    gpio_put(25, LOW);
+  } else {
+    gpio_put(25, HIGH);
+  }
+
   // LCD コントロールレジスタのMSBによりLCD有効でない場合は動作をスルー
   if (*(io + 0x40) & 0b10000000) {
     scaline_counter -= cc_dec;
-    gpio_put(25, HIGH);
   } else {
-    gpio_put(25, LOW);
     return;
   }
+
   uint8_t *t_FF44 = io + 0x44;
   //　スキャンラインの右端に達したらlyをインクリメントしてスキャンラインナンバーを下に進める
   if (scaline_counter <= 0) {
     *t_FF44 = *t_FF44 + 1;
     scaline_counter += 456;
-  }
-  // 非ブランクゾーンの場合の処理
-  if (*t_FF44 < 144) {
-    mode_sw();
-  } else if (*t_FF44 > 153) {
-    *t_FF44 = 0;
-  } else if (*t_FF44 = 144) {
-    *(io + 0x0F) = *(io + 0x0F) | 0b00010000; // v-blank割り込み発生
+    // 非ブランクゾーンの場合の処理
+    if (*t_FF44 < 144) {
+      //display_scanline();
+      display_tile(0);
+      //mode_sw();
+    } else if (*t_FF44 == 144) {
+      //mode_sw();
+      *(io + 0x0F) = *(io + 0x0F) | 0b00010000; // v-blank割り込み発生
+    } else {
+      *t_FF44 = 0;
+    }
   }
 }
-
+/*
+      gpio_put(25, HIGH);
+      delay(100);
+      gpio_put(25, LOW);
+*/
 // bg&wnd フェッチ
 void display_scanline() {
 
   uint8_t *t_FF40 = io + 0x40;
-  uint8_t SCY = *(io + 0x42) << 3; // SCY BGの描画位置
-  uint8_t SCX = *(io + 0x43) << 3; // SCX
-  uint8_t LY  = *(io + 0x44);
-  uint8_t pic_h;
-  uint8_t pic_l;
+  uint8_t SCY = *(io + 0x42); // SCY BGの描画位置
+  uint8_t SCX = *(io + 0x43); // SCX
+  uint16_t LY  = *(io + 0x44);
 
   static uint16_t bg_offset;
   static uint16_t window_offset;
-
   uint16_t base_tile_number;
   uint16_t tile_number;
   uint8_t tile_l;
   uint8_t tile_h;
 
-  for (uint8_t i = 0; i < 20; i++) {
-    base_tile_number = SCX + SCY * 32 + (LY >> 3) << 5; // LY（scanline number）に対応した先頭のtile number
+  for (uint16_t i = 0; i < 0x20; i++) {
+    base_tile_number = (SCX >> 3) + (SCY >> 3) * 32 + ((LY & 0b11111000) << 2); // LY（scanline number）に対応した先頭のtile number
 
-    tile_number = get_byte(0x9800 + base_tile_number); // LYに対応したタイルデータ
-
-    if (*t_FF40 & 0b00010000 == 0b00010000) {
-      tile_l = get_byte(0x8000 + (tile_number << 4) + (LY & 0b00000111) * 2);
-      tile_h = get_byte(0x8001 + (tile_number << 4) + (LY & 0b00000111) * 2);
+    if (*t_FF40 & 0b00001000 == 0b00001000) {
+      tile_number = get_byte(0x9C00 + base_tile_number + i); // LYに対応したタイルデータ
     } else {
-      tile_l = get_byte(0x8800 + ((int8_t)tile_number << 4) + (LY & 0b00000111) * 2);
-      tile_h = get_byte(0x8801 + ((int8_t)tile_number << 4) + (LY & 0b00000111) * 2);
+      //gpio_put(25, HIGH);
+      //delay(100);
+      //gpio_put(25, LOW);
+      tile_number = get_byte(0x9800 + base_tile_number + i); // LYに対応したタイルデータ
+    }
+    if (*t_FF40 & 0b00010000 == 0b00010000) {
+      tile_l = get_byte(0x8000 + (tile_number << 4) + ((LY + (SCY & 0b00000111)) & 0b00000111) * 2);
+      tile_h = get_byte(0x8001 + (tile_number << 4) + ((LY + (SCY & 0b00000111)) & 0b00000111) * 2);
+    } else {
+      tile_l = get_byte(0x8800 + ((int8_t)tile_number << 4) + ((LY + (SCY & 0b00000111)) & 0b00000111) * 2); //怪しい
+      tile_h = get_byte(0x8801 + ((int8_t)tile_number << 4) + ((LY + (SCY & 0b00000111)) & 0b00000111) * 2);
     }
 
-    uint16_t t =  i * 12;
+    for (int n = 0; n < 8; n++) {
 
-    for (int n = 0; n < 4; n++) {
+      uint8_t *tmp_0 = FIFO_bg_wnd + 0 + 2 * n + i * 16;
+      uint8_t *tmp_1 = FIFO_bg_wnd + 1 + 2 * n + i * 16;
 
-      uint8_t *tmp_0 = FIFO_bg_wnd + 0 + 3 * n + t;
-      uint8_t *tmp_1 = FIFO_bg_wnd + 1 + 3 * n + t;
-      uint8_t *tmp_2 = FIFO_bg_wnd + 2 + 3 * n + t;
-      
       *tmp_0 = 0b00000000;
       *tmp_1 = 0b00000000;
-      *tmp_2 = 0b00000000;
-      
-      switch ((tile_l & (0b11000000 >> 2 * n)) >> (6 - 2 * n)) {
-        //case 0b00:
-          //*tmp_0 = 0b00000000;
-          //*tmp_1 = 0b00000000;
-          //*tmp_2 = 0b00000000;
-          //break;
-        case 0b01:
-          //*tmp_0 = 0b00000000;
-          *tmp_1 = 0b00000011;
-          *tmp_2 = 0b00110011;
-          break;
-        case 0b10:
-          *tmp_0 = 0b00110011;
-          *tmp_1 = 0b00110000;
-          //*tmp_2 = 0b00000000;
-          break;
-        case 0b11:
-          *tmp_0 = 0b00110011;
-          *tmp_1 = 0b00110011;
-          *tmp_2 = 0b00110011;
-          break;
+
+      if (tile_l & (0b10000000 >> n)) {
+        *tmp_0 = 0b00011000;
+        *tmp_1 = 0b11000011;
       }
-      switch ((tile_h & (0b11000000 >> 2 * n)) >> (6 - 2 * n)) {
-        //case 0b00:
-        //*tmp_0 |= 0b00000000;
-        //*tmp_1 |= 0b00000000;
-        //*tmp_2 |= 0b00000000;
-        //break;
-        case 0b01:
-          //*tmp_0 |= 0b00000000;
-          *tmp_1 |= 0b00001100;
-          *tmp_2 |= 0b11001100;
-          break;
-        case 0b10:
-          *tmp_0 |= 0b11001100;
-          *tmp_1 |= 0b11000000;
-          //*tmp_2 |= 0b00000000;
-          break;
-        case 0b11:
-          *tmp_0 |= 0b11001100;
-          *tmp_1 |= 0b11001100;
-          *tmp_2 |= 0b11001100;
-          break;
+      if (tile_h & (0b10000000 >> n)) {
+        *tmp_0 |= 0b01100011;
+        *tmp_1 |= 0b00001100;
       }
     }
   }
-
   drowBitMap(LY);
-
-  //digitalWrite(25, LOW);
   return;
 }
