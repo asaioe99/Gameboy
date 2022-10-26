851e7fab1dddbe2b4befe52716a19d611f716223
Yukio Ohata(Alias) <77034428+asaioe99@users.noreply.github.com>
2022-10-27 08:54:18 +0900

    Update debug.ino

--- GameBoypico/debug.ino
+++ GameBoypico/debug.ino
@@ -3,11 +3,6 @@ void display_tile(uint8_t start) {
 
   uint8_t SCY = *(io + 0x42) << 3; // SCY BGの描画位置
   uint8_t SCX = *(io + 0x43) << 3; // SCX
-  uint8_t pic_h;
-  uint8_t pic_l;
-
-  static uint16_t bg_offset;
-  static uint16_t window_offset;
 
   uint16_t base_tile_number;
   uint16_t tile_number;
@@ -15,34 +10,33 @@ void display_tile(uint8_t start) {
   uint8_t tile_h;
 
   for (uint16_t LY = 0; LY < 144 ; LY++) {
-    for (uint16_t i = 0; i < 20; i++) {
+    for (int16_t i = 0; i < 20; i++) {
       base_tile_number = (LY & 0b11111000) << 2; // LY（scanline number）に対応した先頭のtile number
       tile_number = get_byte(0x9800 + base_tile_number + i); // LYに対応したタイルデータ
-      //tile_l = get_byte(0x8000 + (tile_number << 4) + (LY & 0b00000111) * 2);
-      //tile_h = get_byte(0x8001 + (tile_number << 4) + (LY & 0b00000111) * 2);
       tile_l = get_byte(0x8000 + (tile_number << 4) + (LY & 0b00000111) * 2);
       tile_h = get_byte(0x8001 + (tile_number << 4) + (LY & 0b00000111) * 2);
 
+      uint8_t *tmp_0 = FIFO_bg_wnd + (i << 4);
+      uint8_t *tmp_1 = tmp_0 + 1;
+      
       for (int n = 0; n < 8; n++) {
-
-        uint8_t *tmp_0 = FIFO_bg_wnd + 0 + 2 * n + i * 16;
-        uint8_t *tmp_1 = FIFO_bg_wnd + 1 + 2 * n + i * 16;
+        tmp_0 += 2;
+        tmp_1 += 2;
 
         *tmp_0 = 0b00000000;
         *tmp_1 = 0b00000000;
 
         if (tile_l & (0b10000000 >> n)) {
-          *tmp_0 = 0b00011000;
-          *tmp_1 = 0b11000011;
+          *tmp_0 = 0b00111001;
+          *tmp_1 = 0b11000111;
         }
         if (tile_h & (0b10000000 >> n)) {
-          *tmp_0 |= 0b01100011;
-          *tmp_1 |= 0b00001100;
+          *tmp_0 |= 0b11000110;
+          *tmp_1 |= 0b00011000;
         }
       }
-      drowBitMap(LY);
     }
-
-    return;
+    drowBitMap(LY);
   }
+  return;
 }
